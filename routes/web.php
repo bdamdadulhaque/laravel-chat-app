618911b94485b4d5d7740494b7f05ee08f2693cd
bdamdadulhaque <bd.amdadul@gmail.com>
2021-01-03 16:23:20 +0600

    user login, register and showing user list in admin panel

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,26 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('user.app');
 });
+// user register
+Route::get('register', [RegisterController::class, 'register'])->name('register');
+Route::post('register', [RegisterController::class, 'storeUser']);
+
+// user login
+Route::get('login', [LoginController::class, 'login'])->name('login');
+Route::post('login', [LoginController::class, 'authenticate']);
+Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+
+// user dashboard
+Route::get('dashboard', [LoginController::class, 'dashboard'])->name('dashboard');
+
+// admin dasgboard
+Route::get('admin-dashboard', [AdminController::class, 'adminDashboard'])->name('admin-dashboard');
+Route::get('user-list', [AdminController::class, 'userList'])->name('user-list');
